8d906639cdf5d4aeedf237850e71e7504ec36793
Pavel-3DO <76993146+Pavel-3DO@users.noreply.github.com>
2021-06-23 21:08:41 +0300

    z1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:3,
+        speed:500,
+        autoplay:true,
+        waitForAnimate:false,
+    });
+});
