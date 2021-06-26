3e9854c2dec436d7efcf3af211d8963f25beaa3d
Pavel-3DO <76993146+Pavel-3DO@users.noreply.github.com>
2021-06-26 19:23:50 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
     $('.slider').slick({
-        
         dots:true,
         adaptiveHeight:true,
         slidesToShow:3,
+        speed:500,
+        autoplay:true,
+        waitForAnimate:false,
+    });
+});
+$(document).ready(function () {
+    $('.slider2').slick({
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
         speed:500,
         autoplay:true,
         waitForAnimate:false,
